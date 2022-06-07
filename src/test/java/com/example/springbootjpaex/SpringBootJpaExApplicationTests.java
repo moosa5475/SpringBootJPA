8f0026b3b183792708ea75134cb9ee871cca1dfc
moosa5475 <102378148+moosa5475@users.noreply.github.com>
2022-06-07 23:19:17 +0530

    test

---
+++ src/test/java/com/example/springbootjpaex/SpringBootJpaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
